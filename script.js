5b82d32c2fe375c31d382ef42b40e84a8cdd294c
Siddharth Jain <sidojain30705@gmail.com>
2024-01-31 17:20:16 +0530

    Fixed operators before operands bug

--- script.js
+++ script.js
@@ -5,39 +5,62 @@ let string = '';
 let arr = Array.from(bottons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
-        if(e.target.innerHTML == '^'){
-            string += ' ** ';
-            input.value += ' ^ ';
-        }
-        else if(e.target.innerHTML == 'x'){
-            string += ' * ';
-            input.value += ' x ';
-        }
-        else if(e.target.innerHTML == '÷'){
-            string += ' / ';
-            input.value += ' ÷ ';
-        }
-        else if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string = '';
-            input.value = string;
-        }
-        else if(e.target.innerHTML == '&lt;='){
-            if(string.charAt(string.length - 1) == ' ')
-                string = string.substring(0, string.length - 1);
-            if(string.charAt(string.length - 1) == '*' && string.charAt(string.length - 2) == '*')
-                string = string.substring(0, string.length - 2);
-            string = string.substring(0, string.length - 1);
-            if(string == ' ')
+        if(string != ''){
+            if(e.target.innerHTML == '^'){
+                string += ' ** ';
+                input.value += ' ^ ';
+            }
+            else if(e.target.innerHTML == 'x'){
+                string += ' * ';
+                input.value += ' x ';
+            }
+            else if(e.target.innerHTML == '÷'){
+                string += ' / ';
+                input.value += ' ÷ ';
+            }
+            else if(e.target.innerHTML == '='){
+                string = eval(string);
+                input.value = string;
+            }
+            else if(e.target.innerHTML == 'AC'){
                 string = '';
-            input.value = string.replace('**', '^').replace('*', 'x').replace('/', '÷');
-        }
-        else if(e.target.innerHTML == '%' || e.target.innerHTML == '+' || e.target.innerHTML == '-'){
-            string += ' ' + e.target.innerHTML + ' ';
-            input.value += ' ' + e.target.innerHTML + ' ';
+                input.value = string;
+            }
+            else if(e.target.innerHTML == '&lt;='){
+                if(string.charAt(string.length - 1) == ' ')
+                    string = string.substring(0, string.length - 1);
+                if(string.charAt(string.length - 1) == '*' && string.charAt(string.length - 2) == '*')
+                    string = string.substring(0, string.length - 2);
+                string = string.substring(0, string.length - 1);
+                if(string == ' ')
+                    string = '';
+                input.value = string.replace('**', '^').replace('*', 'x').replace('/', '÷');
+            }
+            else if(e.target.innerHTML == '%' || e.target.innerHTML == '+' || e.target.innerHTML == '-'){
+                string += ' ' + e.target.innerHTML + ' ';
+                input.value += ' ' + e.target.innerHTML + ' ';
+            }
+            else{
+                if(e.target.innerHTML == '.'){
+                    temp = string.split(' ');
+                    let count = 0;
+                    for (let i = 0; i < temp[temp.length-1].length; i++){
+                        if(temp[temp.length-1].charAt(i) == '.'){
+                            count++;
+                            break;
+                        }
+                    }
+    
+                    if(count == 0){
+                        string += e.target.innerHTML;
+                        input.value += e.target.innerHTML;
+                    }
+                }
+                else{
+                string += e.target.innerHTML;
+                input.value += e.target.innerHTML;
+                }
+            }
         }
         else{
             if(e.target.innerHTML == '.'){
@@ -55,7 +78,7 @@ arr.forEach(button => {
                     input.value += e.target.innerHTML;
                 }
             }
-            else{
+            else if(e.target.innerHTML != '%' && e.target.innerHTML != '+' && e.target.innerHTML != '-' && e.target.innerHTML != 'x' && e.target.innerHTML != '÷' && e.target.innerHTML != '^'){
             string += e.target.innerHTML;
             input.value += e.target.innerHTML;
             }
