f070d43a5ba7a24f128183b701274cbe73d109cb
Siddharth Jain <sidojain30705@gmail.com>
2024-01-31 15:09:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('inputBox');
+let bottons = document.querySelectorAll('button')
+
+let string = '';
+let arr = Array.from(bottons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '^'){
+            string += ' ** ';
+            input.value += ' ^ ';
+        }
+        else if(e.target.innerHTML == 'x'){
+            string += ' * ';
+            input.value += ' x ';
+        }
+        else if(e.target.innerHTML == '÷'){
+            string += ' / ';
+            input.value += ' ÷ ';
+        }
+        else if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = '';
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '&lt;='){
+            if(string.charAt(string.length - 1) == ' ')
+                string = string.substring(0, string.length - 1);
+            if(string.charAt(string.length - 1) == '*' && string.charAt(string.length - 2) == '*')
+                string = string.substring(0, string.length - 2);
+            string = string.substring(0, string.length - 1);
+            if(string == ' ')
+                string = '';
+            input.value = string.replace('**', '^').replace('*', 'x').replace('/', '÷');
+        }
+        else if(e.target.innerHTML == '%' || e.target.innerHTML == '+' || e.target.innerHTML == '-'){
+            string += ' ' + e.target.innerHTML + ' ';
+            input.value += ' ' + e.target.innerHTML + ' ';
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value += e.target.innerHTML;
+        }
+    })
+})
