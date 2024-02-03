15b82f69845fda5442e73a6b05953079f1efe1a5
Siddharth Jain <sidojain30705@gmail.com>
2024-02-03 14:16:41 +0530

    Made cleaner code

--- script.js
+++ script.js
@@ -5,58 +5,62 @@ let arr = Array.from(bottons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if(input.value != ''){
-            if(e.target.innerHTML == '^')
-                if(input.value.slice(-1) != ' ')
+            if(e.target.innerHTML === '^')
+                if(input.value.slice(-1) !== ' ')
                     input.value += ' ^ ';
                 else
                     input.value += '';
-            else if(e.target.innerHTML == 'x')
-                if(input.value.slice(-1) != ' ')
+            else if(e.target.innerHTML === 'x')
+                if(input.value.slice(-1) !== ' ')
                     input.value += ' x ';
                 else
                     input.value += '';
-            else if(e.target.innerHTML == '÷')
-                if(input.value.slice(-1) != ' ')
+            else if(e.target.innerHTML === '÷')
+                if(input.value.slice(-1) !== ' ')
                     input.value += ' ÷ ';
                 else
                     input.value += '';
-            else if(e.target.innerHTML == '%')
-                if(input.value.slice(-1) != ' ')
+            else if(e.target.innerHTML === '%')
+                if(input.value.slice(-1) !== ' ')
                     input.value += ' ' + e.target.innerHTML + ' ';
                 else
                     input.value += '';
-            else if(e.target.innerHTML == '+' || e.target.innerHTML == '-')
+            else if(e.target.innerHTML === '+' || e.target.innerHTML === '-')
                 input.value += ' ' + e.target.innerHTML + ' ';
-            else if(e.target.innerHTML == 'AC')
+            else if(e.target.innerHTML === 'AC')
                 input.value = '';
-            else if(e.target.innerHTML == '='){
-                input.value = eval(input.value.replace('^', '**').replace('÷', '/').replace('x', '*')).toString();
+            else if(e.target.innerHTML === '='){
+                input.value = eval(input.value.replaceAll('^', '**').replaceAll('÷', '/').replaceAll('x', '*'));
                 if(input.value.includes('e') || input.value.length > 6)
-                    input.value = (Number.parseFloat(input.value)).toExponential(4);
-                else
-                    input.value = (Number.parseFloat(input.value)).toFixed(4);
+                    input.value = parseFloat(input.value).toExponential(4);
+                else if(input.value.includes('.'))
+                    input.value = parseFloat(input.value).toFixed(4);
             }
-            else if(e.target.innerHTML == '&lt;='){
-                if(input.value.slice(-1) == ' ')
-                    input.value = input.value.substring(0, input.value.length - 1);
-                if(input.value.slice(-1) == '^')
-                    input.value = input.value.substring(0, input.value.length - 1);
-                input.value = input.value.substring(0, input.value.length - 1);
-                if(input.value == ' ')
+            else if(e.target.innerHTML === '&lt;='){
+                if(input.value === 'Infinity' || input.value === 'NaN')
                     input.value = '';
-                input.value = input.value.replace('**', '^').replace('*', 'x').replace('/', '÷');
+                else{
+                    if(input.value.slice(-1) === ' ')
+                        input.value = input.value.substring(0, input.value.length - 1);
+                    if(input.value.slice(-1) === '^')
+                        input.value = input.value.substring(0, input.value.length - 1);
+                    input.value = input.value.substring(0, input.value.length - 1);
+                    if(input.value === ' ')
+                        input.value = '';
+                    input.value = input.value.replace('**', '^').replace('*', 'x').replace('/', '÷');
+                }
             }
             else{
-                if(e.target.innerHTML == '.'){
+                if(e.target.innerHTML === '.'){
                     temp = input.value.split(' ');
                     let count = 0;
                     for (let i = 0; i < temp[temp.length-1].length; i++){
-                        if(temp[temp.length-1].charAt(i) == '.'){
+                        if(temp[temp.length-1].charAt(i) === '.'){
                             count++;
                             break;
                         }
                     }
-                    if(count == 0)
+                    if(count === 0)
                         input.value += e.target.innerHTML;
                 }
                 else
@@ -64,20 +68,20 @@ arr.forEach(button => {
             }
         }
         else{
-            if(e.target.innerHTML == '.'){
+            if(e.target.innerHTML === '.'){
                 temp = input.value.split(' ');
                 let count = 0;
                 for (let i = 0; i < temp[temp.length-1].length; i++){
-                    if(temp[temp.length-1].charAt(i) == '.'){
+                    if(temp[temp.length-1].charAt(i) === '.'){
                         count++;
                         break;
                     }
                 }
-                if(count == 0)
+                if(count === 0)
                     input.value += e.target.innerHTML;
             }
-            else if(e.target.innerHTML != '%' && e.target.innerHTML != '+' && e.target.innerHTML != 'x' && e.target.innerHTML != '÷' && e.target.innerHTML != '^' && e.target.innerHTML != 'AC' && e.target.innerHTML != '&lt;=' && e.target.innerHTML != '=')
-                if(e.target.innerHTML == '-')
+            else if(e.target.innerHTML !== '%' && e.target.innerHTML !== '+' && e.target.innerHTML !== 'x' && e.target.innerHTML !== '÷' && e.target.innerHTML !== '^' && e.target.innerHTML !== 'AC' && e.target.innerHTML !== '&lt;=' && e.target.innerHTML !== '=')
+                if(e.target.innerHTML === '-')
                     input.value += ' ' + e.target.innerHTML + ' ';
                 else
                     input.value += e.target.innerHTML;
